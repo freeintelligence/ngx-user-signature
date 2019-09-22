7e4bf4a333ee36e19eb50a6ca19bd6eb1eb45941
freeintelligence <free04381@gmail.com>
2019-09-22 02:47:31 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-signature-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-user-signature-app';
+}
